7611a7914fbaa735a90ac632a620ebe2b339731f
hadinnn <mhafizhmuhyiddin_2428240127@mhs.mdp.ac.id>
2026-04-14 12:00:35 +0700

    Commit Kedua

--- script.js
+++ script.js
@@ -6,4 +6,28 @@ function simpan(){
     console.log(namaBarang.value);
     console.log(jumlah.value);
     console.log(keterangan.value);
+
+    if(localStorage.getItem("daftarBelanja") ===  null){
+        localStorage.setItem("daftarBelanja", "[]");
+    }
+
+    let data = JSON.parse(localStorage.getItem("daftarBelanja"))
+    console.log(data);
+
+    data.push({
+        namaBarang: namaBarang.value,
+        jumlah: jumlah.value,
+        keterangan: keterangan.value
+    })
+    console.log(data);
+
+    localStorage.setItem("daftarBelanja", JSON.stringify(data));
+
+    tampil();
+}
+
+function tampil(){
+    let hasil = JSON.parse(localStorage.getItem("daftarBelanja"));
+
+    document.getElementById("list-belanja")
 }
