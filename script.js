f8ab24e8ad7c31498bc1f09349003bf5da8db30f
hadinnn <mhafizhmuhyiddin_2428240127@mhs.mdp.ac.id>
2026-04-14 11:48:33 +0700

    Commit Pertama

---
+++ script.js
@@ -0,0 +1,9 @@
+let namaBarang = document.getElementById("namaBarang");
+let jumlah = document.getElementById("jumlah");
+let keterangan = document.getElementById("keterangan");
+
+function simpan(){
+    console.log(namaBarang.value);
+    console.log(jumlah.value);
+    console.log(keterangan.value);
+}
